b05d61eefb278fa17794a28987a4d0146daa43a9
ms-Shifu <mailshifu@gmail.com>
2019-12-18 11:52:50 +0300

    Update V2__Add_admin.sql

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,6 +1,6 @@
 insert into usr (id, activation_code, active, address, email, password, phone_number, username)
     values
-    (3, null, true, 'admin', 'admin', 'viper51072C', 'admin', 'admin'),
+    (3, null, true, 'admin', 'admin', '', 'admin', 'admin'),
     (4, null, true, 'u', 'u', 'p', 'u', 'u');
 
 insert into user_roles (user_id, roles)
